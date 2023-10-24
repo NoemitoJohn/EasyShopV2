6dba1e788741a9b739e8162cf21ad7584206f690
NoemitoJohn <129826378+NoemitoJohn@users.noreply.github.com>
2023-10-25 00:28:18 +0800

    Clean checkout controller

--- backend/controller/checkoutController.js
+++ backend/controller/checkoutController.js
@@ -52,6 +52,7 @@ const createOrder = async (_cart, _total, _transaction_id, _user_id) =>{
             
         } catch (error) {
             if(error) t.rollback()
+            console.log(error)
             return false
         }
         
@@ -75,67 +76,6 @@ const createOrder = async (_cart, _total, _transaction_id, _user_id) =>{
         if(error) t.rollback()
         return false
     }
-}
-
-
-const testCheckout = async (req, res) =>{
-    if(!req.user){
-        return res.json({status: 400, message: `Please Login`})
-    }
-    
-    // return console.log(JSON.parse(`[{"cart_id":1},{"cart_id":2},{"cart_id":3},{"cart_id":4}]`))
-    
-    const carts = await DB.Cart.findAll({
-        attributes : [ 'id', 'quantity'], 
-        
-        where : {
-            user_id : req.user.id
-        },
-        
-        include : [
-            {
-                model : DB.Product, 
-                require : true, 
-                attributes : ['id', 'name', 'price'],
-                include: [{
-                    model : DB.ProductInfo,
-                    require: true,
-                    attributes: ['img_url']
-                }]
-            },
-        ]})
-        
-        
-        
-        const product_items = []
-        for (const item of carts) {
-            // const t = await DB.instance.transaction()
-            const images = JSON.parse(item.product.products_info.img_url)
-            const thumbnail = images[0]
-            const productObject = {
-                product : {
-                    id : item.product.id,
-                    name : item.product.name,
-                    quantity : item.quantity,
-                    price : item.product.price,
-                    thumbnail : thumbnail,
-                    
-                }
-            }
-            product_items.push(productObject)
-            
-        }
-        const productsStr = JSON.stringify(product_items)
-        const productParse = JSON.parse(productsStr)
-        
-        console.log(productsStr)
-        const order = await DB.Order.create({
-            product_item : productParse,
-            total : 100,
-            transaction_id : 1020,
-            user_id : 1
-        })
-        res.json(order)
 }
     
     
@@ -190,7 +130,7 @@ const checkout = async (req, res) => {
         res.send(strpSession.url)
             
     } catch (error) {
-        throw error
+        console.log(error)
     }
             
             
@@ -200,7 +140,7 @@ const webhook = async (req, res) => {
     const endpointSecret = "whsec_6afb0d281c9bad3ec7ab1267a13b333576d2db499e638e0e8adc7225e21ab6cd";
     
     const sig = req.headers['stripe-signature'];
-    // console.log('Weeeeeeeebhoook')
+
     let event;
     
     try {
@@ -211,37 +151,43 @@ const webhook = async (req, res) => {
         return;
     }
     
-    // console.log('event.type', event.type)
-    // Handle the event
     if (event.type == 'checkout.session.completed' ) {
         
         
         const checkoutCompleted = event.data.object;
         
-        if(checkoutCompleted.payment_status !== 'paid') return res.send('')
+        if(checkoutCompleted.payment_status !== 'paid') return 
         
         
         const data = checkoutCompleted.metadata
-        console.log(checkoutCompleted)
-        // return res.json(checkoutCompleted)
-        const cart = await DB.Cart.findAll({
-            attributes : [ 'id', 'quantity'], 
-            where : {
-                user_id : data.user_id
-            },
-            include : [
-                {
-                    model : DB.Product, 
-                    require : true, 
-                    attributes : ['id', 'name', 'price'],
-                    include: [{
-                        model : DB.ProductInfo,
-                        require: true,
-                        attributes: ['img_url']
-                    }]
+        
+        let cart;
+        
+        try {
+            
+            cart = await DB.Cart.findAll({
+                attributes : [ 'id', 'quantity'], 
+                where : {
+                    user_id : data.user_id
                 },
-            ] 
-        })
+                include : [
+                    {
+                        model : DB.Product, 
+                        require : true, 
+                        attributes : ['id', 'name', 'price'],
+                        include: [{
+                            model : DB.ProductInfo,
+                            require: true,
+                            attributes: ['img_url']
+                        }]
+                    },
+                ] 
+            })
+
+        } catch (error) {
+            if(error)
+                console.error(error)
+        }
         
         const order = await createOrder(cart, checkoutCompleted.amount_total, checkoutCompleted.id, data.user_id)
         
